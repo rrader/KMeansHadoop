709b367bbc5cbf03fdca5622bee2810e91f00dd5
Roman Rader <roman.rader@gmail.com>
2014-05-04 15:06:57 +0300

    Classifier

---
+++ KMeansClassifier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# KMeansMapper.py
+
+import sys
+from KMeansMapper import centerList, distanceOfTwoProperty
+
+if __name__ == "__main__":
+    print(distanceOfTwoProperty(centerList[0], centerList[1]))
+    for line in sys.stdin:
+        val = [float(i) for i in line.strip().split(",")[1:24*7+1]]
+        dists = [distanceOfTwoProperty(val, center) for center in centerList]
+        center_id, distance = min(enumerate(dists), key=lambda x:x[1])
+        print(center_id)
